6dfebf160f7dafe3ea085bc452e09a9df7833f1f
daeun <daeun900@naver.com>
2024-09-13 15:33:07 +0900

    240913_휴대폰본인인증 기능

--- src/screens/LectureCerti.js
+++ src/screens/LectureCerti.js
@@ -3,7 +3,9 @@ import styled from "styled-components/native";
 import { Text, View, Image, Alert} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useRoute } from '@react-navigation/native';
+import { WebView } from 'react-native-webview';
 import axios from 'axios';
+import { parseString } from 'react-native-xml2js';
 
 const LectureCerti = ({ navigation }) => {
   const insets = useSafeAreaInsets(); //아이폰 노치 문제 해결
@@ -29,7 +31,6 @@ const LectureCerti = ({ navigation }) => {
   
     const USRDT = new Date().toISOString(); // 현재 시간 사용
     const CLASS_AGENT_PK = `${LectureCode},${LectureTermeIdx}`;
-  
     try {
       const response = await axios.post("https://emonotp.hrdkorea.or.kr/api/v2/otp_accredit", 
         new URLSearchParams({
@@ -92,8 +93,8 @@ const LectureCerti = ({ navigation }) => {
                   });
                 
                   alert("인증처리가 완료되었습니다.");
-                  console.log('player_captcha_session으로 보낸 데이터: ', ChapterNum, LectureCode, StudySeq, ChapterSeq);
-                  console.log(response.data.result); // 서버 응답의 result 필드를 출력
+                  //console.log('player_captcha_session으로 보낸 데이터: ', ChapterNum, LectureCode, StudySeq, ChapterSeq);
+                  //console.log(response.data.result); 
                 } catch (error) {
                   console.error("인증 처리 중 오류 발생:", error);
                   alert("인증 처리 중 오류가 발생하였습니다.");
@@ -137,6 +138,125 @@ const LectureCerti = ({ navigation }) => {
       Alert.alert("네트워크 오류", "인증 중 오류가 발생하였습니다. 휴대폰 인증을 시도하세요.");
     }
   };
+  
+  const [encData, setEncData] = useState(null);
+  const [webviewVisible, setWebviewVisible] = useState(false);
+  
+  //휴대폰 인증
+  const getEncData = async () => {
+    try {
+      const response = await axios.post('https://hrdelms.com/mobileTest/create_encoded_data.php', {
+        lectureCode: LectureCode
+      });
+      setEncData(response.data.encData);  // 서버에서 암호화된 데이터 받음
+      setWebviewVisible(true);  // 인증 시작
+    } catch (error) {
+      console.error('Error fetching enc data:', error);
+    }
+  };
+
+  // XML 파싱을 Promise로 변환
+  const parseXml = (xmlData) => {
+    return new Promise((resolve, reject) => {
+      parseString(xmlData, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
+
+  //인증성공 시
+  const handleMobileSubmit = async () => {
+    const USRDT = new Date().toISOString(); // 현재 시간 사용
+    const CLASS_AGENT_PK = `${LectureCode},${LectureTermeIdx}`;
+
+    try {
+      const response = await axios.post("https://emon.hrdkorea.or.kr/EAIServer/SOURCE/ExConn/LMS/pSubOtpLog.jsp", 
+        new URLSearchParams({
+        AGTID: AgtId,
+        USRID: Id,
+        COURSE_AGENT_PK: LectureCode,
+        CLASS_AGENT_PK,
+        EVAL_CD: EvalCd,
+        EVAL_TYPE: EvalType,
+        CLASS_TME: EvalCd === '00' ? '00' : ChapterNumberZero,
+        m_Ret: 'T',
+        m_retCD: '000000',
+        m_trnID: TrnId,
+        m_trnDT: USRDT
+      }), {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        }
+      });
+
+        const result = await parseXml(response.data);  // parseXml로 XML 파싱
+        //console.log(result);  // 전체 파싱 결과 출력
+        const retVal = result?.Result?.RetVal?.[0];
+        //console.log(retVal)
+          if (retVal === "101") {
+              //인증성공시
+              if (EvalCd === '00') {
+               // 입과 본인 인증 시
+                try {
+                  await axios.post("https://hrdelms.com/mobileTest/player_cert_insert.php", {
+                    id: Id,
+                    lectureCode: LectureCode,
+                    studySeq: StudySeq,
+                    certType: 'user',
+                    AGTID: AgtId,
+                    COURSE_AGENT_PK: LectureCode,
+                    CLASS_AGENT_PK,
+                    m_Ret: 'T',
+                    m_retCD: '000000',
+                    m_trnID: TrnId,
+                    m_trnDT: USRDT
+                  });
+        
+                  alert("입과시 인증처리가 완료되었습니다.\n 한번 더 인증처리가 필요합니다.");
+        
+                } catch (error) {
+                  console.error("인증 처리 중 오류 발생:", error);
+                  alert("인증 처리 중 오류가 발생하였습니다.");
+                }
+              } else {
+                // 입과 외 인증
+                try {
+                  await axios.post("https://hrdelms.com/mobileTest/player_captcha_session.php", {
+                    chapterNum: ChapterNum,
+                    lectureCode: LectureCode,
+                    studySeq: StudySeq,
+                    chapterSeq: ChapterSeq
+                  });
+        
+                  alert("인증처리가 완료되었습니다.");
+                  console.log('player_captcha_session으로 보낸 데이터: ', ChapterNum, LectureCode, StudySeq, ChapterSeq);
+                } catch (error) {
+                  console.error("인증 처리 중 오류 발생:", error);
+                  alert("인증 처리 중 오류가 발생하였습니다.");
+                }
+              }
+        
+              // 인증 성공 후 강의 플레이어 화면으로 이동
+              navigation.navigate("LecturePlayer", {
+                LectureCode: LectureCode,
+                StudySeq: StudySeq,
+                ChapterSeq: ChapterSeq,
+                ContentsIdx: ContentsIdx,
+                PlayMode: PlayMode,
+                ProgressStep: ProgressStep
+              });
+            } else {
+              console.error("인증 실패", retVal);
+            }
+        
+          } catch (error) {
+            console.error("네트워크 오류:", error);
+          }
+        };
   
 
 const Title = styled.Text`
@@ -220,7 +340,10 @@ const Num = styled.Text`
     font-size: 16px;
 `
   return (
+    
     <View insets={insets} style={{flex:1}}>
+        {!webviewVisible ? (
+        <>
        <Title>{ ContentsName }</Title>
        <SubTitle>[1차시_어린이집 평가 운영체계]</SubTitle>
        <Container contentContainerStyle={{ paddingBottom: insets.bottom + 30}}>
@@ -238,8 +361,7 @@ const Num = styled.Text`
             </InputWrap>
             <SmallTxt>*mOTP외 다른 방법을 원하시는 분은 아래 대체인증을 눌러주세요</SmallTxt>
             <ButtonWrap>
-                <CertiBtn><MidTxt>휴대폰 인증</MidTxt></CertiBtn>
-                <CertiBtn><MidTxt>아이핀  인증</MidTxt></CertiBtn>
+                <CertiBtn onPress={getEncData}><MidTxt>휴대폰 인증</MidTxt></CertiBtn>
             </ButtonWrap>
             <ExImg source={require('../../assets/mOTP.png')}/>
             <SmallTxt>*위 이미지는 예시용 이미지입니다.</SmallTxt>
@@ -252,6 +374,32 @@ const Num = styled.Text`
                 <MidTxt>mOTP사용이 불가한 경우 다른 방법을 사용하시기 바랍니다.</MidTxt>
             </NumTxtWrap>
        </Container>
+       </>
+        ) : (
+        <WebView
+        source={{
+          uri: 'https://nice.checkplus.co.kr/CheckPlusSafeModel/checkplus.cb',
+          method: 'POST',
+          body: `m=checkplusSerivce&EncodeData=${encData}`,  // 서버에서 받은 암호화된 데이터 전송
+        }}
+        onNavigationStateChange={(event) => {
+          //console.log('test1')
+          if (event.url.includes('checkplus_success')) {
+            // 성공 페이지 도달 시 처리
+            alert('인증 성공');
+            setWebviewVisible(false);  // 인증 완료 후 WebView 닫기
+
+            handleMobileSubmit();
+            //console.log('test2')
+          } else if (event.url.includes('checkplus_fail')) {
+            // 실패 페이지 도달 시 처리
+            alert('인증 실패');
+            setWebviewVisible(false);  // 인증 실패 시 WebView 닫기
+            //console.log('test3')
+          }
+        }}
+      />
+      )}
     </View>
   );
 };

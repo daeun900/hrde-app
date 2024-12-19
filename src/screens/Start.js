bbc8c053c702392d474e3c2e77784638d86b25b7
daeun <daeun900@naver.com>
2024-12-19 15:02:28 +0900

    241219_교육원 선택 방법 변경(코드)

--- src/screens/Start.js
+++ src/screens/Start.js
@@ -7,9 +7,6 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import DropDownPicker from 'react-native-dropdown-picker';
 import axios from "axios"; 
 
-
-
-
 const Container = styled.View`
   flex: 1;
   justify-content:space-between;
@@ -21,7 +18,7 @@ const Container = styled.View`
 `;
 
 const SelectSec = styled.View`
-margin-top: 100px;
+margin-top: 80px;
 ` 
 const LoginSec = styled.View`
 ` 
@@ -47,32 +44,10 @@ margin-bottom: 10px;
 const Start = ({ navigation }) => {
   const insets = useSafeAreaInsets(); //아이폰 노치 문제 해결
 
-  // selectbox
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
-  const [items, setItems] = useState([]);
-
-   // 교육원 목록 가져오기
    const { setDomain } = useDomain(); 
+   const [code, setCode] = useState(""); 
 
-   const fetchEduCenters = async () => {
-    try {
-      const response = await axios.get("https://app.hrdelms.com/edu_center.php");
-      const centers = response.data;
-
-      const dropdownItems = Object.keys(centers).map((key) => ({
-        label: centers[key],
-        value: key,
-      }));
-
-      setItems(dropdownItems);
-    } catch (error) { 
-      console.error(error);
-      Alert.alert("오류", "교육원 목록을 가져오는 중 문제가 발생했습니다.");
-    }
-  };
-
-  // 선택된 교육원의 도메인 받아오기
+  // 입력한 코드 교육원의 도메인 받아오기
   const fetchDomain = async (centerCode) => {
     try {
       const response = await axios.post("https://app.hrdelms.com/edu_center_domain.php", {
@@ -83,7 +58,7 @@ const Start = ({ navigation }) => {
       if (domain && domain !== "No matching domain found") {
         setDomain(domain);
       } else {
-        Alert.alert("오류", "해당 교육원의 URL을 찾을 수 없습니다.");
+        Alert.alert("오류", "해당 교육원을 찾을 수 없습니다.");
       }
     } catch (error) {
       console.error(error);
@@ -91,24 +66,20 @@ const Start = ({ navigation }) => {
     }
   };
 
-  // DropDown에서 선택된 값이 변경될 때 API 호출
-  useEffect(() => {
-    if (value) {
-      fetchDomain(value);
+  const handleLoginPress = async () => {
+    if (!code) {
+      Alert.alert("", "교육원 코드를 입력해주세요."); // 교육원 코드 미입력 시 경고창
+      return;
     }
-  }, [value]);
-
-  useEffect(() => {
-    fetchEduCenters();
-  }, []);
-
-  const handleLoginPress = () => {
-    if (!value) {
-      Alert.alert("", "교육원을 선택해주세요.");  // 교육원 미선택 시 경고창
-    } else {
-      navigation.navigate("Signin");  // 교육원 선택 후 로그인 가능
+  
+    try {
+      await fetchDomain(code); // 도메인 가져오기
+      navigation.navigate("Signin"); // 도메인 설정 후 로그인 화면으로 이동
+    } catch (error) {
+      Alert.alert("오류", "도메인 설정 중 문제가 발생했습니다.");
     }
   };
+  
 
   return (
     <ImageBackground 
@@ -118,22 +89,12 @@ const Start = ({ navigation }) => {
       >
       <Container insets={insets}>
         <SelectSec>
-          <BigTxt>교육원을 선택해주세요.</BigTxt>
-          <DropDownPicker
-            open={open}
-            value={value}
-            items={items}
-            setOpen={setOpen}
-            setValue={setValue}
-            setItems={setItems}
-            listMode="MODAL"   
-            maxHeight={300} 
-            placeholder="교육원을 선택하세요"
-            containerStyle={{ marginTop: 15, borderColor: '#EEEEEE' , zIndex:10 }}
-            style={{ paddingHorizontal: 20, paddingVertical: 20, borderColor: '#EEEEEE'}}
-            textStyle={{ fontSize: 16 }}
-            placeholderStyle={{ color: '#9a9a9a' }}
-            dropDownContainerStyle={{ borderColor: '#EEEEEE', paddingHorizontal: 10 }}
+          <BigTxt>교육원 코드를 입력해주세요.</BigTxt>
+          <Input
+            placeholder="교육원 코드를 입력하세요"
+            value={code}
+            onChangeText={setCode}
+            containerStyle={{ marginTop:-10,borderColor: "#EEEEEE" }}
           />
         </SelectSec>
        <LoginSec>
